3d2d2f99589dafb8f3bc2d0ef900d0a5fbec123c
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-17 22:55:49 +0530

    Updated code

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,21 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string result = "";
+            int modindex;
+            int retriveIndex;
+            foreach (char character in text)
+            {
+                if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122))
+                {
+                    modindex = (character <= 90) ? character - 65 : character - 97;
+                    retriveIndex = (character <= 90) ? 65 : 97;
+                    result += (char)((modindex + shiftKey) % 26 + retriveIndex);
+                }
+                else
+                    result += character;
+            }
+            return result;
         }
     }
 }
